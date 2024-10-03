dd64dc1b35fa90118628d219e07e44fc97dec694
Ekaterina Fomina <eafomina2022@mail.ru>
2024-10-03 15:46:31 +0300

    update models.py

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -6,8 +6,16 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    ...
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return (f'{self.user}: {self.text}')
+    
+    def likes_count(self):
+        return self.likes.count()
 
 # для доп. задания
 # class PostImage(models.Model):
@@ -15,8 +23,18 @@ class Post(models.Model):
 
 
 class Like(models.Model):
-    ...
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f'{self.user} likes post: {self.post.text}')
 
 
 class Comment(models.Model):
-    ...
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (f'Post: {self.post.text} User: {self.user} Comment: {self.text}')
