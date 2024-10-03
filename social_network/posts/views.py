21ee49f32f2cd0adad4fe476a9545edb2caab810
Ekaterina Fomina <eafomina2022@mail.ru>
2024-10-04 02:50:43 +0300

    update views.py

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from posts.models import Comment, Like, Post
+from posts.permissions import IsOwnerOrReadOnly
+from posts.serializers import CommentSerializer, PostSerializer
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class LikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        if not Like.objects.filter(post=post, user=request.user).exists():
+            Like.objects.create(post=post, user=request.user)
+        return Response(status=status.HTTP_200_OK)
