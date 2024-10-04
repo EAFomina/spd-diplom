f3a40d00d1c6d4eafc736e97fb43d3c22a2c6de4
Ekaterina Fomina <eafomina2022@mail.ru>
2024-10-04 03:47:16 +0300

    change settings.py

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 # настройте STATIC_ROOT, MEDIA_URL и MEDIA_ROOT
 
 # Default primary key field type
